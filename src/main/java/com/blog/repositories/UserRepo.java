a7dfc1f7994496926db506699a2ca570c499293d
shezad <coetus@98gmial.com>
2023-10-18 22:44:16 +0530

    intial commit

---
+++ src/main/java/com/blog/repositories/UserRepo.java
@@ -0,0 +1,12 @@
+package com.blog.repositories;
+
+import java.util.Optional;
+
+import com.blog.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Integer>{
+		
+	
+	Optional<User> findByEmail(String email);
+}
---
+++ src/test/java/com/blog/BlogAppApisApplicationTests.java
@@ -0,0 +1,17 @@
+package com.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppApisApplicationTests {
+
+
+
+	@Test
+	void contextLoads() {
+
+	}
+
+
+}
